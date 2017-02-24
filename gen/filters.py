4647a8557fdfa7dbc8c2f92b452fdb8ee290759b
Jeronimo Bezerra <jab@amlight.net>
2017-02-24 12:02:25 -0300

    Added support for packetin and packetout filters based on dpid and port

--- gen/filters.py
+++ gen/filters.py
@@ -7,6 +7,7 @@
 
 import tcpiplib.tcpip
 import tcpiplib.packet
+import of10
 
 
 def filter_msg(msg):
@@ -32,6 +33,10 @@ def filter_msg(msg):
 
     # Filter Ethertypes from PacketIn/Out messages
     if ethertype_filters(msg):
+        return True
+
+    # Filter PacketIn/Out based on DPID and Port
+    if dpid_filters(msg):
         return True
 
     return False
@@ -120,4 +125,98 @@ def ethertype_filters(msg):
                 if next_protocol == int(protocol, 16):
                     return True
 
+    return False
+
+
+def dpid_filters(msg):
+    """
+        Filter PacketIn and PacketOut messages based on DPID and ports
+        Sanitizer filter (-F), entry "filters", "packetIn_filter" or
+          "packetOut_filter"
+          If switch_dpid AND in_port are Any, don't filter (print it)
+          If switch_dpid OR in_port are NOT Any, print only what matches the
+            most specific (filter everything else)
+        Args:
+            msg: class OFMessage
+        Returns:
+            False: Don' filter packet (print it)
+            True: Filter it (don't print)
+    """
+
+    # It has to be a PacketOut or PacketIn
+    if msg.ofp.type not in [10, 13]:
+        return False
+
+    # It has to be a LLDP packet
+    idx = 0
+    if isinstance(msg.ofp.data[idx], tcpiplib.packet.Ethernet):
+        next_protocol = msg.ofp.data[idx].protocol
+        idx += 1
+        if isinstance(msg.ofp.data[idx], tcpiplib.packet.VLAN):
+            next_protocol = msg.ofp.data[idx].protocol
+        try:
+            if next_protocol not in [35020, 35138]:
+                return False
+        except KeyError:
+            return False
+
+    try:
+        # If it is a PacketIn ...
+        if msg.ofp.type in [10]:
+            # It has to have a packetIn_filter filter
+            filters = msg.sanitizer['filters']['packetIn_filter']
+            filter_port = filters['in_port']
+        # If it a PacketOut...
+        else:
+            # It has to have a packetOut_filter filter
+            filters = msg.sanitizer['filters']['packetOut_filter']
+            filter_port = filters['out_port']
+
+        filter_dpid = filters['switch_dpid']
+
+    except KeyError:
+        return False
+    if not len(filters):
+        return False
+
+    # Was switch_dpid or in_port specified by user?
+    if filter_dpid in ['any', 'Any', 'ANY']:
+        if filter_port in ['any', 'Any', 'ANY']:
+            return False
+
+    # If we got here, it means we have content to avoid printing
+    print_it = False
+    lldp_msg = msg.ofp.data[idx+1]
+    try:
+        switch_dpid = filter_dpid.split(':')[1]
+    except:
+        switch_dpid = filter_dpid
+
+    if print_switch_dpid(switch_dpid, lldp_msg.c_id):
+        if msg.ofp.type in [10]:
+            if print_port(filter_port, str(msg.ofp.in_port)):
+                print_it = True
+        else:
+            if print_port(filter_port, str(lldp_msg.p_id)):
+                print_it = True
+
+    if print_it:
+        return False
+    else:
+        return True
+
+
+def print_switch_dpid(filter_dpid, packet_dpid):
+    try:
+        p_dpid = packet_dpid.split(':')[1]
+    except:
+        p_dpid = packet_dpid
+    if filter_dpid in [p_dpid, 'Any', 'any', 'ANY']:
+        return True
+    return False
+
+
+def print_port(filter_port, packet_port):
+    if filter_port in [packet_port, 'Any', 'any', 'ANY']:
+        return True
     return False
